e6dbdf16dce2148668d71a6e74879b305100d8e2
CS-Brito <cs-brito@hotmail.com>
2023-04-29 08:36:23 -0300

    Agregado de carpeta plantillas

--- dietetica/urls.py
+++ dietetica/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from dietetica.views import pruebaTemplate
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('template1/', pruebaTemplate),
 ]
--- dietetica/views.py
+++ dietetica/views.py
@@ -1,1 +1,13 @@
 from django.http import HttpResponse
+from django.template import Context, Template
+
+
+def pruebaTemplate(self):
+    
+    miHtml = open("C:/Users/cs-br/OneDrive/Documentos/Repositorio/Proyecto-Final/dietetica/AppDietetica/plantillas/template1.html")
+    plantilla = Template(miHtml.read())
+    miHtml.close
+    miContexto = Context()
+    documento = plantilla.render(miContexto)
+    return HttpResponse(documento)
+    
